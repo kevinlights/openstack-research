1345993f89d0821da7440e36f8711f609af7587c
kevinlights <guangjinhai@163.com>
2025-08-23 09:44:39 +0800

    network simple

--- ubuntu2204/kvm.sh
+++ ubuntu2204/kvm.sh
@@ -1,9 +1,13 @@
 
 
 ifconfig -a
-dhclient enp0s8
+# dhclient enp0s8
 ifconfig
 sudo service ssh start
+
+# https://www.oryoy.com/news/ubuntu-16-04-ssh-fu-wu-zi-qi-dong-pei-zhi-quan-gong-lve-qing-song-shi-xian-yuan-cheng-deng-lu-wu-you-a14611105.html
+sudo systemctl enable ssh
+
 
 
 # install kvm packages
@@ -51,9 +55,14 @@ virt-manager
 # https://download.cirros-cloud.net/
 sudo ls /var/lib/libvirt/images/
 wget https://download.cirros-cloud.net/0.6.3/cirros-0.6.3-x86_64-disk.img
-sudo mv cirros-0.6.3-x86_64-disk.img /var/lib/libvirt/images/
+# sudo mv cirros-0.6.3-x86_64-disk.img /var/lib/libvirt/images/
 wget https://download.cirros-cloud.net/0.3.0/cirros-0.3.0-x86_64-disk.img
+wget https://download.cirros-cloud.net/0.3.3/cirros-0.3.3-x86_64-disk.img
+sudo cp ~/cirros-0.3.3-x86_64-disk.img /var/lib/libvirt/images/vm1.img
+sudo cp ~/cirros-0.3.3-x86_64-disk.img /var/lib/libvirt/images/vm2.img
 
+sudo cp ~/cirros-0.3.0-x86_64-disk.img /var/lib/libvirt/images/vm3.img
+sudo cp ~/cirros-0.3.0-x86_64-disk.img /var/lib/libvirt/images/vm4.img
 
 virsh list
 
@@ -61,4 +70,6 @@ virsh list
 # libvirt+  5111     1 99 22:12 ?        00:04:04 qemu-system-x86_64 -enable-kvm -name generic -S -machine pc-i440fx-xenial,accel=kvm,usb=off -cpu Westmere -m 100 -realtime mlock=off -smp 1,sockets=1,cores=1,threads=1 -uuid 3c5f0e5b-ee60-484f-8cf7-a59f408458a0 -no-user-config -nodefaults -chardev socket,id=charmonitor,path=/var/lib/libvirt/qemu/domain-generic/monitor.sock,server,nowait -mon chardev=charmonitor,id=monitor,mode=control -rtc base=utc,driftfix=slew -global kvm-pit.lost_tick_policy=discard -no-hpet -no-shutdown -global PIIX4_PM.disable_s3=1 -global PIIX4_PM.disable_s4=1 -boot strict=on -device ich9-usb-ehci1,id=usb,bus=pci.0,addr=0x6.0x7 -device ich9-usb-uhci1,masterbus=usb.0,firstport=0,bus=pci.0,multifunction=on,addr=0x6 -device ich9-usb-uhci2,masterbus=usb.0,firstport=2,bus=pci.0,addr=0x6.0x1 -device ich9-usb-uhci3,masterbus=usb.0,firstport=4,bus=pci.0,addr=0x6.0x2 -device virtio-serial-pci,id=virtio-serial0,bus=pci.0,addr=0x5 -drive file=/var/lib/libvirt/images/cirros-0.6.3-x86_64-disk.img,format=qcow2,if=none,id=drive-ide0-0-0 -device ide-hd,bus=ide.0,unit=0,drive=drive-ide0-0-0,id=ide0-0-0,bootindex=1 -netdev tap,fd=26,id=hostnet0 -device rtl8139,netdev=hostnet0,id=net0,mac=52:54:00:8f:b2:4c,bus=pci.0,addr=0x3 -chardev pty,id=charserial0 -device isa-serial,chardev=charserial0,id=serial0 -chardev spicevmc,id=charchannel0,name=vdagent -device virtserialport,bus=virtio-serial0.0,nr=1,chardev=charchannel0,id=channel0,name=com.redhat.spice.0 -spice port=5900,addr=127.0.0.1,disable-ticketing,image-compression=off,seamless-migration=on -device qxl-vga,id=video0,ram_size=67108864,vram_size=67108864,vgamem_mb=16,bus=pci.0,addr=0x2 -device intel-hda,id=sound0,bus=pci.0,addr=0x4 -device hda-duplex,id=sound0-codec0,bus=sound0.0,cad=0 -chardev spicevmc,id=charredir0,name=usbredir -device usb-redir,chardev=charredir0,id=redir0 -chardev spicevmc,id=charredir1,name=usbredir -device usb-redir,chardev=charredir1,id=redir1 -device virtio-balloon-pci,id=balloon0,bus=pci.0,addr=0x7 -msg timestamp=on
 
 
-
+# https://cloud-images.ubuntu.com/minimal/releases/
+wget https://cloud-images.ubuntu.com/minimal/releases/bionic/release/ubuntu-18.04-minimal-cloudimg-amd64.img
+sudo cp ubuntu-18.04-minimal-cloudimg-amd64.img /var/lib/libvirt/images/vm3.img
---
+++ ubuntu2204/network.sh
@@ -0,0 +1,243 @@
+cat /etc/network/interfaces
+
+# # This file describes the network interfaces available on your system
+# # and how to activate them. For more information, see interfaces(5).
+
+# source /etc/network/interfaces.d/*
+
+# # The loopback network interface
+# auto lo
+# iface lo inet loopback
+
+# # The primary network interface
+# auto enp0s3
+# iface enp0s3 inet dhcp
+
+# app@ubuntu:/dev$ ifconfig
+# enp0s3    Link encap:Ethernet  HWaddr 08:00:27:f5:0d:99
+#           inet addr:10.0.2.15  Bcast:10.0.2.255  Mask:255.255.255.0
+#           inet6 addr: fe80::a00:27ff:fef5:d99/64 Scope:Link
+#           UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1
+#           RX packets:21 errors:0 dropped:0 overruns:0 frame:0
+#           TX packets:27 errors:0 dropped:0 overruns:0 carrier:0
+#           collisions:0 txqueuelen:1000
+#           RX bytes:4220 (4.2 KB)  TX bytes:3376 (3.3 KB)
+
+# lo        Link encap:Local Loopback
+#           inet addr:127.0.0.1  Mask:255.0.0.0
+#           inet6 addr: ::1/128 Scope:Host
+#           UP LOOPBACK RUNNING  MTU:65536  Metric:1
+#           RX packets:13853 errors:0 dropped:0 overruns:0 frame:0
+#           TX packets:13853 errors:0 dropped:0 overruns:0 carrier:0
+#           collisions:0 txqueuelen:1
+#           RX bytes:17337296 (17.3 MB)  TX bytes:17337296 (17.3 MB)
+
+# virbr0    Link encap:Ethernet  HWaddr 52:54:00:bb:92:d0
+#           inet addr:192.168.122.1  Bcast:192.168.122.255  Mask:255.255.255.0
+#           UP BROADCAST MULTICAST  MTU:1500  Metric:1
+#           RX packets:0 errors:0 dropped:0 overruns:0 frame:0
+#           TX packets:0 errors:0 dropped:0 overruns:0 carrier:0
+#           collisions:0 txqueuelen:1000
+#           RX bytes:0 (0.0 B)  TX bytes:0 (0.0 B)
+
+sudo cp /etc/network/interfaces /etc/network/interfaces.bak
+
+cat <<EOF | sudo tee /etc/network/interfaces
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+# The primary network interface
+auto enp0s3
+iface enp0s3 inet manual
+
+auto br0
+iface br0 inet dhcp
+    bridge_stp off
+    bridge_waitport 0
+    bridge_fd 0
+    bridge_ports enp0s3
+EOF
+# bridge_ports 需要连接到 host only 的网卡上，否则 kvm 实例将无法连接到互联网
+
+sudo reboot
+
+# app@ubuntu:~$ ifconfig
+# br0       Link encap:Ethernet  HWaddr 08:00:27:f5:0d:99
+#           inet addr:10.0.2.15  Bcast:10.0.2.255  Mask:255.255.255.0
+#           inet6 addr: fe80::a00:27ff:fef5:d99/64 Scope:Link
+#           UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1
+#           RX packets:57 errors:0 dropped:0 overruns:0 frame:0
+#           TX packets:77 errors:0 dropped:0 overruns:0 carrier:0
+#           collisions:0 txqueuelen:1000
+#           RX bytes:8870 (8.8 KB)  TX bytes:13706 (13.7 KB)
+
+# enp0s3    Link encap:Ethernet  HWaddr 08:00:27:f5:0d:99
+#           UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1
+#           RX packets:57 errors:0 dropped:0 overruns:0 frame:0
+#           TX packets:77 errors:0 dropped:0 overruns:0 carrier:0
+#           collisions:0 txqueuelen:1000
+#           RX bytes:9668 (9.6 KB)  TX bytes:13818 (13.8 KB)
+
+# lo        Link encap:Local Loopback
+#           inet addr:127.0.0.1  Mask:255.0.0.0
+#           inet6 addr: ::1/128 Scope:Host
+#           UP LOOPBACK RUNNING  MTU:65536  Metric:1
+#           RX packets:160 errors:0 dropped:0 overruns:0 frame:0
+#           TX packets:160 errors:0 dropped:0 overruns:0 carrier:0
+#           collisions:0 txqueuelen:1
+#           RX bytes:11840 (11.8 KB)  TX bytes:11840 (11.8 KB)
+
+# virbr0    Link encap:Ethernet  HWaddr 52:54:00:bb:92:d0
+#           inet addr:192.168.122.1  Bcast:192.168.122.255  Mask:255.255.255.0
+#           UP BROADCAST MULTICAST  MTU:1500  Metric:1
+#           RX packets:0 errors:0 dropped:0 overruns:0 frame:0
+#           TX packets:0 errors:0 dropped:0 overruns:0 carrier:0
+#           collisions:0 txqueuelen:1000
+#           RX bytes:0 (0.0 B)  TX bytes:0 (0.0 B)
+
+
+brctl show
+
+# bridge name     bridge id               STP enabled     interfaces
+# br0             8000.080027f50d99       no              enp0s3
+# virbr0          8000.525400bb92d0       yes             virbr0-nic
+
+
+
+virsh list --all
+#  Id    Name                           State
+# ----------------------------------------------------
+#  -     VM1                            shut off
+#  -     VM2                            shut off
+#  -     VM3                            shut off
+#  -     VM4                            shut off
+
+
+
+virsh start VM1
+
+
+brctl show
+
+# bridge name     bridge id               STP enabled     interfaces
+# br0             8000.080027f50d99       no              enp0s3
+#                                                         vnet0
+# virbr0          8000.525400bb92d0       yes             virbr0-nic
+
+
+virsh domiflist VM1
+# Interface  Type       Source     Model       MAC
+# -------------------------------------------------------
+# vnet0      bridge     br0        rtl8139     52:54:00:ca:ce:65
+virsh domiflist VM2
+
+# Interface  Type       Source     Model       MAC
+# -------------------------------------------------------
+# -          bridge     br0        rtl8139     52:54:00:e9:00:67
+
+# go to VM1
+ifconfig
+# eth0 10.0.2.8
+ping 8.8.8.8
+ping www.bing.com
+
+# ok
+
+
+# 将 VM1 的 nic 设置成 default NAT
+
+brctl show
+# bridge name     bridge id               STP enabled     interfaces
+# br0             8000.080027f50d99       no              enp0s3
+#                                                         vnet1
+# virbr0          8000.525400bb92d0       yes             virbr0-nic
+#                                                         vnet0
+
+
+# app@ubuntu:~$ virsh domiflist VM1
+# Interface  Type       Source     Model       MAC
+# -------------------------------------------------------
+# vnet0      network    default    rtl8139     52:54:00:78:be:94
+
+
+ps -ef|grep dnsmasq
+
+app@ubuntu:~$ ps -ef|grep dnsmasq
+# libvirt+  1331     1  0 09:20 ?        00:00:00 /usr/sbin/dnsmasq --conf-file=/var/lib/libvirt/dnsmasq/default.conf --leasefile-ro --dhcp-script=/usr/lib/libvirt/libvirt_leaseshelper
+# root      1332  1331  0 09:20 ?        00:00:00 /usr/sbin/dnsmasq --conf-file=/var/lib/libvirt/dnsmasq/default.conf --leasefile-ro --dhcp-script=/usr/lib/libvirt/libvirt_leaseshelper
+
+app@ubuntu:/var/lib/libvirt/dnsmasq$ cat virbr0.status
+# [
+#     {
+#         "ip-address": "192.168.122.225",
+#         "mac-address": "52:54:00:78:be:94",
+#         "hostname": "cirros",
+#         "client-id": "01:52:54:00:78:be:94",
+#         "expiry-time": 1755916269
+#     }
+# ]
+
+app@ubuntu:/var/lib/libvirt/dnsmasq$ virsh console VM1
+# Connected to domain VM1
+# Escape character is ^]
+
+# login as 'cirros' user. default password: 'cubswin:)'. use 'sudo' for root.
+# cirros login: cirros
+# Password:
+$ ifconfig
+# eth0      Link encap:Ethernet  HWaddr 52:54:00:78:BE:94
+#           inet addr:192.168.122.225  Bcast:192.168.122.255  Mask:255.255.255.0
+#           inet6 addr: fe80::5054:ff:fe78:be94/64 Scope:Link
+#           UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1
+#           RX packets:20 errors:0 dropped:0 overruns:0 frame:0
+#           TX packets:103 errors:0 dropped:0 overruns:0 carrier:0
+#           collisions:0 txqueuelen:1000
+#           RX bytes:1750 (1.7 KiB)  TX bytes:7892 (7.7 KiB)
+#           Interrupt:11 Base address:0xa000
+
+# lo        Link encap:Local Loopback
+#           inet addr:127.0.0.1  Mask:255.0.0.0
+#           inet6 addr: ::1/128 Scope:Host
+#           UP LOOPBACK RUNNING  MTU:16436  Metric:1
+#           RX packets:0 errors:0 dropped:0 overruns:0 frame:0
+#           TX packets:0 errors:0 dropped:0 overruns:0 carrier:0
+#           collisions:0 txqueuelen:0
+#           RX bytes:0 (0.0 B)  TX bytes:0 (0.0 B)
+
+app@ubuntu:/var/lib/libvirt/dnsmasq$ ssh cirros@192.168.122.225
+# cirros@192.168.122.225's password:
+$ ifconfig
+# eth0      Link encap:Ethernet  HWaddr 52:54:00:78:BE:94
+#           inet addr:192.168.122.225  Bcast:192.168.122.255  Mask:255.255.255.0
+#           inet6 addr: fe80::5054:ff:fe78:be94/64 Scope:Link
+#           UP BROADCAST RUNNING MULTICAST  MTU:1500  Metric:1
+#           RX packets:75 errors:0 dropped:0 overruns:0 frame:0
+#           TX packets:141 errors:0 dropped:0 overruns:0 carrier:0
+#           collisions:0 txqueuelen:1000
+#           RX bytes:9550 (9.3 KiB)  TX bytes:13560 (13.2 KiB)
+#           Interrupt:11 Base address:0xa000
+
+# lo        Link encap:Local Loopback
+#           inet addr:127.0.0.1  Mask:255.0.0.0
+#           inet6 addr: ::1/128 Scope:Host
+#           UP LOOPBACK RUNNING  MTU:16436  Metric:1
+#           RX packets:0 errors:0 dropped:0 overruns:0 frame:0
+#           TX packets:0 errors:0 dropped:0 overruns:0 carrier:0
+#           collisions:0 txqueuelen:0
+#           RX bytes:0 (0.0 B)  TX bytes:0 (0.0 B)
+
+$ ping www.bing.com
+# PING www.bing.com (202.89.233.101): 56 data bytes
+# 64 bytes from 202.89.233.101: seq=0 ttl=114 time=23.857 ms
+# 64 bytes from 202.89.233.101: seq=1 ttl=114 time=22.856 ms
+# 64 bytes from 202.89.233.101: seq=2 ttl=114 time=23.350 ms
+
+
+
+
