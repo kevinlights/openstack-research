5de6945e17e505491f2224eb10777a01e134cc3f
kevinlights <guangjinhai@163.com>
2025-08-30 22:16:39 +0800

    add cmds

---
+++ kolla-ansible/cmds.sh
@@ -0,0 +1,13 @@
+
+source /etc/kolla/admin-openrc.sh
+
+openstack server list
+
+ip netns
+sudo ip netns exec qdhcp-0777e878-82bd-4873-a372-602b9b4e93de ip a
+
+docker exec -it openvswitch_vswitchd ip a
+docker exec -it openvswitch_vswitchd ovs-vsctl show
+
+brctl show
+
